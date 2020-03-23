3df18f7036f0fd5063a74d81a44be68b6868eff3
nishionemui <anlihvosion227@gmail.com>
2020-03-23 13:13:32 +0900

    2

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,7 +1,7 @@
 $(function() {
   // テキストボックスへ Datepicker を仕掛ける
   $("#xxdate").datepicker({
-    buttonImage: "(画像URL)",        // カレンダーアイコン画像
+    buttonImage: "file:///Users/nemui/Downloads/%E3%82%AB%E3%83%AC%E3%83%B3%E3%83%80%E3%83%BC%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B38.svg",        // カレンダーアイコン画像
     buttonText: "カレンダーから選択", // ツールチップ表示文言
     buttonImageOnly: true,           // 画像として表示
     showOn: "both"                   // カレンダー呼び出し元の定義
