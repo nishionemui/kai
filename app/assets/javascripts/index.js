9e9655f86680e0363d25dd48420209d53a13c6fb
nishionemui <anlihvosion227@gmail.com>
2020-03-23 12:33:31 +0900

    1

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,30 @@
+$(function() {
+  // テキストボックスへ Datepicker を仕掛ける
+  $("#xxdate").datepicker({
+    buttonImage: "(画像URL)",        // カレンダーアイコン画像
+    buttonText: "カレンダーから選択", // ツールチップ表示文言
+    buttonImageOnly: true,           // 画像として表示
+    showOn: "both"                   // カレンダー呼び出し元の定義
+  });
+ 
+  // 日本語化
+  $.datepicker.regional['ja'] = {
+    closeText: '閉じる',
+    prevText: '<前',
+    nextText: '次>',
+    currentText: '今日',
+    monthNames: ['1月','2月','3月','4月','5月','6月',
+    '7月','8月','9月','10月','11月','12月'],
+    monthNamesShort: ['1月','2月','3月','4月','5月','6月',
+    '7月','8月','9月','10月','11月','12月'],
+    dayNames: ['日曜日','月曜日','火曜日','水曜日','木曜日','金曜日','土曜日'],
+    dayNamesShort: ['日','月','火','水','木','金','土'],
+    dayNamesMin: ['日','月','火','水','木','金','土'],
+    weekHeader: '週',
+    dateFormat: 'yy/mm/dd',
+    firstDay: 0,
+    isRTL: false,
+    showMonthAfterYear: true,
+    yearSuffix: '年'};
+  $.datepicker.setDefaults($.datepicker.regional['ja']);
+});
