719419f884d8f90d17bbe816fd25bba3c22fa84f
nishionemui <anlihvosion227@gmail.com>
2020-03-24 20:12:28 +0900

    9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :tel, :email, :encrypted_password, :address, :Facility])
   end
+
+  def after_sign_out_path_for(resource)
+    '/users/sign_in' # サインアウト後のリダイレクト先URL
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   
   root "posts#index"
-  resources :users, only: [:index, :edit, :update]
+  resources :users, only: [:index, :edit, :update, :destroy]
 end
