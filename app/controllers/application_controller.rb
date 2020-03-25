a6d2d12943aac202726734f58c44ef13224ff9b1
nishionemui <anlihvosion227@gmail.com>
2020-03-25 14:46:45 +0900

    10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :tel, :email, :encrypted_password, :address, :Facility])
   end
 
-  def after_sign_out_path_for(resource)
-    '/users/sign_in' # サインアウト後のリダイレクト先URL
-  end
+  # def after_sign_out_path_for(resource)
+  #   '/users/sign_in' # サインアウト後のリダイレクト先URL
+  # end
 end
